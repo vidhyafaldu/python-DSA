d6e210c0d5028428f385f6631287c8b77553f29e
Vidya Faldu <falduvidya@gmail.com>
2026-09-13 14:33:39 +0530

    operators_demo.py

--- operators_demo.py
+++ operators_demo.py
@@ -2,7 +2,6 @@
 
 a = 10
 b = 3
-
 print(" ARITHMETIC OPERATORS  ")
 
 print("a + b =", a + b)    # Addition
@@ -23,40 +22,28 @@ print("a < b  :", a < b)   # Less than
 print("a >= b :", a >= b)  # Greater than or equal
 print("a <= b :", a <= b)  # Less than or equal
 
-
 print("\n  ASSIGNMENT OPERATORS  ")
-
 x = 10
 print("x =", x)
-
 x += 5
 print("x += 5 :", x)
-
 x -= 2
 print("x -= 2 :", x)
-
 x *= 2
 print("x *= 2 :", x)
-
 x /= 2
 print("x /= 2 :", x)
 
-
 print("\n LOGICAL OPERATORS  ")
-
 age = 22
 has_id = True
-
 print("age > 18 and has_id :", age > 18 and has_id)
 print("age < 18 or has_id   :", age < 18 or has_id)
 print("not has_id           :", not has_id)
 
-
 print("\n BITWISE OPERATORS  ")
-
 p = 10
 q = 3
-
 print("p & q  =", p & q)   # AND
 print("p | q  =", p | q)   # OR
 print("p ^ q  =", p ^ q)   # XOR
@@ -64,28 +51,22 @@ print("~p     =", ~p)      # NOT
 print("p << 1 =", p << 1)  # Left shift
 print("p >> 1 =", p >> 1)  # Right shift
 
-
 print("\n  MEMBERSHIP OPERATORS  ")
-
 students = ["Vidya", "Riya", "Neha"]
-
 print("'Vidya' in students     :", "Vidya" in students)
 print("'Aisha' in students     :", "Aisha" in students)
 print("'Aisha' not in students:", "Aisha" not in students)
-
 
 print("\n IDENTITY OPERATORS ")
 
 list1 = [1, 2, 3]
 list2 = list1
 list3 = [1, 2, 3]
-
 print("list1 is list2 :", list1 is list2)
 print("list1 is list3 :", list1 is list3)
 print("list1 == list3 :", list1 == list3)
 
 
 print("\n OPERATOR PRECEDENCE  ")
-
 result = 10 + 5 * 2
 print("10 + 5 * 2 =", result)
